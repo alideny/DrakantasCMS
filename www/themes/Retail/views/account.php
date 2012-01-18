f86a874821849965021718b1194e8825287139f5
Asfo <iasfo@hotmail.com>
2012-01-18 12:13:08 -0600

    [22]Olvidé cambiar el mensaje, sin embargo agrego un "to do" para recordar que se debe cambiar esa sección.

--- www/themes/Retail/views/account.php
+++ www/themes/Retail/views/account.php
@@ -7,8 +7,8 @@
 		if($expansion == 2){print "Wrath of the Lich King";}
 		if($expansion == 1){print "The Burning Crusade";}
 		if($expansion == 0){print "Classic";}
-		?></b></p>
-		<p>Bloqueado: <b style="text-transform:capitalize;"><?php ($locked != 0) ? print "Baneado" : print "No Baneado"; ?></b></p>
+		?></b></p><?php /* Buscar un mejor 'formato' para imprimir si está o no bloqueada la cuenta con 'true/false' y un mensaje cambiado de "Bloqueado: Bloqueado" a otro para evitar redundancias*/?>
+		<p>Bloqueado: <b style="text-transform:capitalize;"><?php ($locked != 0) ? print "Bloqueado" : print "No Bloqueado"; ?></b></p>
 		<p>Monedas: <b style="text-transform:capitalize;"><?php echo $coins; ?></b></p>
 		<p>Puntos: <b style="text-transform:capitalize;"><?php echo $points; ?></b></p>
 	</div>
