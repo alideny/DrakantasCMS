73279651cf520b3c32cedcef2ea65f1b164aa051
Asfo <iasfo@hotmail.com>
2012-01-17 20:03:49 -0600

    Update www/models/news_model.php

--- www/models/news_model.php
+++ www/models/news_model.php
@@ -20,17 +20,43 @@ class News_model extends CI_Model {
 	
 	}
 	public function set_news()
-{
-	$this->load->helper('url');
+	{
+		$this->load->helper('url');
+		
+		$slug = url_title($this->input->post('title'), 'dash', TRUE);
+		
+		$data = array(
+			'title' => $this->input->post('title'),
+			'slug' => $slug,
+			'text' => $this->input->post('text')
+		);
+		
+		return $this->db->insert('drak_news', $data);
+	}
 	
-	$slug = url_title($this->input->post('title'), 'dash', TRUE);
+	public function get_total_comments($Id = NULL)
+	{
+		if(is_null($Id))
+		{
+			return false;
+		}
+		else
+		{
+			$query = $this -> db -> get_where('drak_news_comments', array('id_news' => $Id));
+			return $query -> num_rows();
+		}
+	}
 	
-	$data = array(
-		'title' => $this->input->post('title'),
-		'slug' => $slug,
-		'text' => $this->input->post('text')
-	);
-	
-	return $this->db->insert('drak_news', $data);
-}
+	public function get_comments($Id)
+	{
+		if(is_null($Id))
+		{
+			return false;
+		}
+		else
+		{
+			$query = $this -> db -> get_where('drak_news_comments', array('id_news' => $Id));
+			return $query -> row_array();
+		}
+	}
 }
